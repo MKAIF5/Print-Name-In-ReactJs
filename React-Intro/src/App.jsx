72eb10871cf46fe3a0ccb3f973eda99a2568599f
mohammed kaif <mkaif555123@gmail.com>
2024-08-13 01:48:50 +0500

    add more h1 h2 and files exports code write reactjsx

--- React-Intro/src/App.jsx
+++ React-Intro/src/App.jsx
@@ -1,7 +1,15 @@
+import Kaif from "./kaif.jsx"
+
 function App() {
 
   return (
+    <>
    <h1>My Name Is Kaif</h1>
+   <h1>Sir Shehzad Bohat Achey Teacher Hain</h1>
+   <h1>aur code likhne ka dil hai mera</h1>
+   <h2>bade hokey inshallah bohat bada admi banu ga</h2>
+   <Kaif/>
+   </>
   )
 }
 
---
+++ React-Intro/src/kaif.jsx
@@ -0,0 +1,7 @@
+function Kaif(){
+    return(
+        <h1>Files Export Karkey Likha hai Direct Nhi</h1>
+    )
+}
+
+export default Kaif
